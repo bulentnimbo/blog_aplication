6bded16ad4cde0b7d75517fd8fb7f0f1e25190e5
BulentEr <bulent.er@nimbo.com.tr>
2022-09-02 22:38:33 +0300

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "dean@example.com", password: "password", password_confirmation: "password" )
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} World!")
+    Post.create(title: "Title #{x}", body: "Body #{x} World!", user_id: User.first.id)
 end
