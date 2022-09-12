7d644ca236e30c14f2d6acdc9bcdeef26ee9e88c
Bulent-Er <111678316+Bulent-Er@users.noreply.github.com>
2022-09-12 13:08:16 +0300

    initial commit

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,21 +1,45 @@
 import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller {
+export default class extends Controller 
+{
   initialize() {
     console.log("initialized");
    }
-  connect() { }
-  toggleForm(event) {
-    console.log("I clicked the edit button");
-    event.preventDefault();
-    event.stopPropagation();
-    const formID = event.params["form"];
-    console.log("I formID the edit  " + formID);
-    const commentBodyID = event.params["body"];
-    const form = document.getElementById(formID);
-    form.classList.toggle("d-none");
-    form.classList.toggle("mt-5");
-    const commentBody = document.getElementById(commentBodyID);
-    commentBody.classList.toggle("d-none");
-  }
+   connect() { }
+   toggleForm(event) {
+     console.log("I clicked the edit button");
+     event.preventDefault();
+     event.stopPropagation();
+     
+     const formID = event.params["form"];
+     const commentBodyID = event.params["body"];
+     const editButtonID = event.params["edit"];
+ 
+     const form = document.getElementById(formID);
+     const commentBody = document.getElementById(commentBodyID);
+     const editButton = document.getElementById(editButtonID);
+    
+     form.classList.toggle("d-none");
+     form.classList.toggle("mt-5");
+     commentBody.classList.toggle("d-none");
+     this.toggleEditButton(editButton);
+   }
+ 
+   toggleEditButton(editButton) 
+   {
+     if ( editButton.innerText === "Edit" ) 
+     {
+       editButton.innerText = "Cancel";
+       this.toggleEditButtonClass(editButton);
+     } else 
+     {
+       editButton.innerText = "Edit"
+       this.toggleEditButtonClass(editButton)
+     }
+   }
+     toggleEditButtonClass(editButton) 
+     {
+       editButton.classList.toggle("btn-secondary");
+       editButton.classList.toggle("btn-warning");
+     }
 }
